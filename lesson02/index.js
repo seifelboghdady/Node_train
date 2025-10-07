a3119f1d715141e4390f487438f6586ba83bc95e
Seif Elboghdady <seifelboghdady@std.mans.edu.eg>
2025-10-07 23:59:09 +0300

    this is complete project in phase 1

--- lesson02/index.js
+++ lesson02/index.js
@@ -1,16 +1,50 @@
-//import file system module to read and write external file
+#!/usr/bin/env node
+import {program} from 'commander';
+import inquirer from 'inquirer';
+import fs from 'node:fs';
+program
+    .command('addName [name] [age] [active]')
+    .description("Create a new user to store in system")
+    .action(()=>{
+    inquirer.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Write Name of Employee',
+      },
+      {
+        type: 'number',
+        name: 'age',
+        message: 'Enter Age',
+      },
+      {
+        type: 'confirm',
+        name: 'active',
+        message: "Is Active",
+        default: true,
+      }
+    ]).then(answers => {
+      let oldData = [];
+      //read old data and store in arr oldData
+      if(fs.existsSync('./data.json')){
+        const fileContent = fs.readFileSync('./data.json', 'utf8');
+        if (fileContent.trim()) {
+          oldData = JSON.parse(fileContent);
+        } 
+      }
+      oldData.push(answers);
 
-const fs = require('node:fs');
-
-//nonblocking
-console.log(1);
-console.log(`This Fs File Content: `+fs.readFileSync('./test.txt'));
-console.log(3);
-//=======================================================
-//blocking
-console.log(1);
-fs.readFile('./test.txt','utf8', ( _ ,data)=>{
-    console.log('This is content of non Blocking data'+ data)
+      fs.writeFile('./data.json',JSON.stringify(oldData),'utf8',(err)=>{
+        if(err){
+          throw Error('The Error is', err)
+        }
+      })
+    });
+    })
+program.command('list').action(()=>{
+  const data = JSON.parse(fs.readFileSync('./data.json', 'utf8'));
+  console.table(data);
 })
-console.log(3)
-//
+
+program.parse(process.argv)
+
